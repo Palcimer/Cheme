f3bfeccdb0d757bd1f9204d31d24b468aed69ad9
Dyd6913 <aorh6913@gmail.com>
2021-05-14 14:07:41 +0900

    GBoard GBoardComment vo 추가
    GBoard GBoardComment vo 추가
    김록희 부재중 권용찬 대리 작성

---
+++ src/gboard/model/vo/GBoard.java
@@ -0,0 +1,86 @@
+package gboard.model.vo;
+
+public class GBoard {
+	 // 그룹게시글번호 
+    private int gBoardNo;
+    // 모임번호 
+    private int groupId;
+    // 그룹게시글제목 
+    private String gBoardTitle;
+    // 그룹게시글내용 
+    private String gBoardContent;
+    // 그룹게시글작성자 
+    private int gBoardWriter;
+    // 그룹게시글게시일 
+    private String gBoardDate;
+    // 그룹게시글파일명(업로드명) 
+    private String gBoardFilename;
+    // 그룹게시글파일명(시스템명) 
+    private String gBoardFilepath;
+	public GBoard() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public GBoard(int gBoardNo, int groupId, String gBoardTitle, String gBoardContent, int gBoardWriter,
+			String gBoardDate, String gBoardFilename, String gBoardFilepath) {
+		super();
+		this.gBoardNo = gBoardNo;
+		this.groupId = groupId;
+		this.gBoardTitle = gBoardTitle;
+		this.gBoardContent = gBoardContent;
+		this.gBoardWriter = gBoardWriter;
+		this.gBoardDate = gBoardDate;
+		this.gBoardFilename = gBoardFilename;
+		this.gBoardFilepath = gBoardFilepath;
+	}
+	public int getgBoardNo() {
+		return gBoardNo;
+	}
+	public void setgBoardNo(int gBoardNo) {
+		this.gBoardNo = gBoardNo;
+	}
+	public int getGroupId() {
+		return groupId;
+	}
+	public void setGroupId(int groupId) {
+		this.groupId = groupId;
+	}
+	public String getgBoardTitle() {
+		return gBoardTitle;
+	}
+	public void setgBoardTitle(String gBoardTitle) {
+		this.gBoardTitle = gBoardTitle;
+	}
+	public String getgBoardContent() {
+		return gBoardContent;
+	}
+	public void setgBoardContent(String gBoardContent) {
+		this.gBoardContent = gBoardContent;
+	}
+	public int getgBoardWriter() {
+		return gBoardWriter;
+	}
+	public void setgBoardWriter(int gBoardWriter) {
+		this.gBoardWriter = gBoardWriter;
+	}
+	public String getgBoardDate() {
+		return gBoardDate;
+	}
+	public void setgBoardDate(String gBoardDate) {
+		this.gBoardDate = gBoardDate;
+	}
+	public String getgBoardFilename() {
+		return gBoardFilename;
+	}
+	public void setgBoardFilename(String gBoardFilename) {
+		this.gBoardFilename = gBoardFilename;
+	}
+	public String getgBoardFilepath() {
+		return gBoardFilepath;
+	}
+	public void setgBoardFilepath(String gBoardFilepath) {
+		this.gBoardFilepath = gBoardFilepath;
+	}
+    
+
+}
---
+++ src/gboardcomment/model/vo/GBoardComment.java
@@ -0,0 +1,76 @@
+package gboardcomment.model.vo;
+
+public class GBoardComment {
+	// 그룹게시판댓글번호 
+    private int gBoardCommentNo;
+    // 그룹게시글번호 
+    private int gBoardNo;
+    // 그룹게시판댓글내용 
+    private String gBoardCommentContent;
+    // 그룹게시판댓글작성자 
+    private int gBoardCommentWriter;
+    // 그룹게시판댓글게시일 
+    private String gBoardCommentDate;
+    // 그룹게시판댓글레벨 
+    private int gBoardCommentLev;
+    // 그룹게시판댓글구분 
+    private int gBoardCommentRef;
+	public GBoardComment() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public GBoardComment(int gBoardCommentNo, int gBoardNo, String gBoardCommentContent, int gBoardCommentWriter,
+			String gBoardCommentDate, int gBoardCommentLev, int gBoardCommentRef) {
+		super();
+		this.gBoardCommentNo = gBoardCommentNo;
+		this.gBoardNo = gBoardNo;
+		this.gBoardCommentContent = gBoardCommentContent;
+		this.gBoardCommentWriter = gBoardCommentWriter;
+		this.gBoardCommentDate = gBoardCommentDate;
+		this.gBoardCommentLev = gBoardCommentLev;
+		this.gBoardCommentRef = gBoardCommentRef;
+	}
+	public int getgBoardCommentNo() {
+		return gBoardCommentNo;
+	}
+	public void setgBoardCommentNo(int gBoardCommentNo) {
+		this.gBoardCommentNo = gBoardCommentNo;
+	}
+	public int getgBoardNo() {
+		return gBoardNo;
+	}
+	public void setgBoardNo(int gBoardNo) {
+		this.gBoardNo = gBoardNo;
+	}
+	public String getgBoardCommentContent() {
+		return gBoardCommentContent;
+	}
+	public void setgBoardCommentContent(String gBoardCommentContent) {
+		this.gBoardCommentContent = gBoardCommentContent;
+	}
+	public int getgBoardCommentWriter() {
+		return gBoardCommentWriter;
+	}
+	public void setgBoardCommentWriter(int gBoardCommentWriter) {
+		this.gBoardCommentWriter = gBoardCommentWriter;
+	}
+	public String getgBoardCommentDate() {
+		return gBoardCommentDate;
+	}
+	public void setgBoardCommentDate(String gBoardCommentDate) {
+		this.gBoardCommentDate = gBoardCommentDate;
+	}
+	public int getgBoardCommentLev() {
+		return gBoardCommentLev;
+	}
+	public void setgBoardCommentLev(int gBoardCommentLev) {
+		this.gBoardCommentLev = gBoardCommentLev;
+	}
+	public int getgBoardCommentRef() {
+		return gBoardCommentRef;
+	}
+	public void setgBoardCommentRef(int gBoardCommentRef) {
+		this.gBoardCommentRef = gBoardCommentRef;
+	}
+    
+}
