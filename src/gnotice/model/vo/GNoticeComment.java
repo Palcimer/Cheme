2959b91fbbd6da3f4292f63e4a9169bf3458369c
bowwow2115 <bowwow2115@gmail.com>
2021-05-14 12:45:14 +0900

    추가 gnotice, gnoticecomment

---
+++ src/gnotice/model/vo/GNoticeComment.java
@@ -0,0 +1,17 @@
+package gnotice.model.vo;
+
+public class GNoticeComment {
+
+	private int gNcNo;
+	private int gNcLev;
+	private int gNcRef;
+	private int noticeNo;
+	private String ncWriter;
+	private String ncContent;
+	public NoticeComment() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
